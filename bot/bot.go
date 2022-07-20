3f569f11c72a0e37e82cee2d4d4ac0e4c0d6771d
optionalCTF <rootparadox@protonmail.com>
2022-07-20 23:20:25 +0100

    wtf is this shit

--- bot/bot.go
+++ bot/bot.go
@@ -3,8 +3,11 @@ package bot
 import (
 	"fmt"
 	"gobot/config"
+	"strconv"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	hiscores "github.com/joey-colon/osrs-hiscores"
 )
 
 var BotId string
@@ -35,10 +38,32 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	// Fields is awful if you require arguments that may have spaces e.g usernames
+	cmdlet := strings.Fields(m.Content)
 	if m.Content[0:1] != config.BotPrefix {
 		return
-	} else if m.Content == "!ping" {
+	} else if cmdlet[0] == "!ping" {
 		_, err := s.ChannelMessageSend(m.ChannelID, "pong")
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else if cmdlet[0] == "!skill" {
+		h := hiscores.NewHiscores()
+
+		stats, err := h.GetPlayerSkillRank(cmdlet[1], cmdlet[2])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		lvl, _ := h.GetPlayerSkillLevel(cmdlet[1], cmdlet[2])
+
+		level := strconv.Itoa(int(lvl))
+		xp := strconv.Itoa(int(stats))
+
+		response := "Username: " + cmdlet[1] + "\nCurrent " + cmdlet[2] + " level: " + level + "\nCurrent " + cmdlet[2] + " XP: " + xp
+		_, err = s.ChannelMessageSend(m.ChannelID, response)
 
 		if err != nil {
 			fmt.Println(err.Error())
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gobot
 
 go 1.13
 
-require github.com/bwmarrin/discordgo v0.25.0
+require (
+	github.com/bwmarrin/discordgo v0.25.0
+	github.com/joey-colon/osrs-hiscores v0.0.0-20210823054940-18b00bcaee2c
+)
