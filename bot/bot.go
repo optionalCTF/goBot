523216484f8dfff05299f5156da69a1e059987c7
optionalCTF <rootparadox@protonmail.com>
2022-07-22 20:23:36 +0100

    Stop breaking things you chimp

--- bot/bot.go
+++ bot/bot.go
@@ -33,9 +33,6 @@ func Start() {
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Content == "" {
-		return
-	}
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
@@ -47,7 +44,11 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 	}
 
+	if m.Content == "" {
+		return
+	}
 	// Fields is awful if you require arguments that may have spaces e.g usernames
+	// This will error if content is empty. Which it will be if user enters a sticker in a bot-readable channel.............
 	cmdlet := strings.Fields(m.Content)
 	if m.Content[0:1] != config.BotPrefix {
 		return
@@ -61,19 +62,20 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 		// OSRS hiscore lookup... This is an awful implementation but eh... who cares
 	} else if cmdlet[0] == "!skill" {
+		username := strings.Join(cmdlet[2:], " ")
 		h := hiscores.NewHiscores()
 
-		stats, err := h.GetPlayerSkillRank(cmdlet[1], cmdlet[2])
+		stats, err := h.GetPlayerSkillRank(username, cmdlet[1])
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		lvl, _ := h.GetPlayerSkillLevel(cmdlet[1], cmdlet[2])
+		lvl, _ := h.GetPlayerSkillLevel(username, cmdlet[1])
 
 		level := strconv.Itoa(int(lvl))
 		xp := strconv.Itoa(int(stats))
 
-		response := "Username: " + cmdlet[1] + "\nCurrent " + cmdlet[2] + " level: " + level + "\nCurrent " + cmdlet[2] + " XP: " + xp
+		response := "Username: " + username + "\nCurrent " + cmdlet[1] + " level: " + level + "\nCurrent " + cmdlet[1] + " XP: " + xp
 		_, err = s.ChannelMessageSend(m.ChannelID, response)
 
 		if err != nil {
